5560d82e93786f682def801df37040a4eb3bb48b
Sebastian Jativa <sebastian.jativa@hotmail.com>
2022-07-14 18:19:55 -0500

    agregado una migracion y regresado a postgres

--- db/config.js
+++ db/config.js
@@ -1,18 +1,17 @@
-const { Sequelize } = require('sequelize');
-
 const { config } = require('../config/config')
-const setupModels = require('../db/models')
-
 
 const USER= encodeURIComponent(config.dbUser)
 const PASSWORD= encodeURIComponent(config.dbPassword)
 const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
-const sequelize = new Sequelize(URI, {
-  dialect: 'postgres',
-  logging: true,
-});
+module.exports = {
+  development: {
+    url: URI,
+    dialect: 'postgres',
+  },
+  production: {
+    url: URI,
+    dialect: 'postgres',
+  }
+}
 
-setupModels(sequelize)
-
-module.exports = sequelize;
